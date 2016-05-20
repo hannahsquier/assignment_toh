5bfa6474e702f8c8df82c63eae6de05322a6d433
Hannah Squier <hannahsquier@gmail.com>
2016-05-19 21:57:30 -0700

    final touches

--- towers-of-hanoi.rb
+++ towers-of-hanoi.rb
@@ -4,41 +4,53 @@ class TowersOfHanoi
   def initialize(towers=3)
     @towers = towers
   end
- 
+  
+  #initialize board
   def set_up_board
     @board = Array.new(@towers, [])
     @board[0] = (1..@towers).to_a
   end
 
+  #play!
   def play
+    puts "Welcome to TOWERS OF HANOI. The goal of the game is to move all the disks 
+      from the leftmost tower to the rightmost tower, adhering to the following 
+      rules: Move only one disk at a time. A larger disk may not be placed ontop 
+      of a smaller disk. To play, chose the tower (1 - #{@towers}) you would 
+      like to move a piece from. Next, chose the tower (1 - #{@towers}) you would like to 
+      move that disk to."
     set_up_board
     get_move
   end
 
+  #ask player where she would like to move from and to
   def get_move
     display_board
-    puts "Which tower would you like to move the top disk from?"
+    puts "Move FROM?"
     from = gets.chomp.to_i - 1
 
-    puts "Which tower would you like to move that piece to? #{(1..@towers).to_a.delete(from)}"
+    puts "Move TO?"
     to = gets.chomp.to_i - 1
 
     if valid_move?(from, to)
       play_move(from, to)
     else
-      puts "That is not a valid choice, please chose a tower between 1 and #{@towers}. Remember no larger piece can go on a smaller piece."
+      puts "NOT VALID. Try again :)"
       get_move
     end
   end
 
+  #define valid move
   def valid_move?(from, to)
-    if from.between?(0, @towers) && to.between?(0, @towers) && from != to && (@board[to].empty? || @board[from][0] < @board[to][0] )
+    if from.between?(0, @towers) && to.between?(0, @towers) && from != to && 
+      (@board[to].empty? || @board[from][0] < @board[to][0] )
       return true
     else
       return false
     end
   end
 
+  #manipulate board to reflect player move choise
   def play_move(from, to)
     to_dup = @board[to].dup
     to_dup.unshift(@board[from][0]) 
@@ -47,25 +59,36 @@ class TowersOfHanoi
   
     
     if won?
+      display_board
       puts "congratulations you won!"
     else
       get_move
     end
   end
 
-  # def display_board
-  #   display_board = Array.new(@towers)
+  #Display the board so that it is easier to visualize
+  def display_board
+    display_board = Array.new(@towers)
     
-  #   @board.each_with_index do |tower, i|
-  #     display_board[i] = [" "] * (@towers - tower.length) + tower
-  #   end
-
-  #   display_board.transpose.each { |row| puts row }
+    @board.each_with_index do |tower, i|
+      display_board[i] = [" "] * (@towers - tower.length) + tower
+    end
     
-  # end
+    puts "__________"
+    puts "\nTOWERS OF HANOI\n"
+    
+    display_board.transpose.each do |row|
+      puts " " + row.join("    ")
+    end
+    
+    puts "---  " * @towers 
+    puts " " + (1..@towers).to_a.join("    ")
+    puts "\n\n"
+  end
 
+  #identify what winning means (all disks in order on last tower)
   def won?
-    if @board.any? { |tower| tower == tower.sort && tower.length == @towers}
+    if @board.last == @board.last.sort && @board.last.length == @towers
       return true
     else
       return false
