44475a66824d329ba0548646162348aaac4f0c2e
Hannah Squier <hannahsquier@gmail.com>
2016-05-22 11:26:27 -0700

    fix valid_move? nil error. Add quit exit command.

--- towers-of-hanoi.rb
+++ towers-of-hanoi.rb
@@ -13,71 +13,104 @@ class TowersOfHanoi
 
   #play!
   def play
-    puts "Welcome to TOWERS OF HANOI. The goal of the game is to move all the disks 
-      from the leftmost tower to the rightmost tower, adhering to the following 
-      rules: Move only one disk at a time. A larger disk may not be placed ontop 
-      of a smaller disk. To play, chose the tower (1 - #{@towers}) you would 
-      like to move a piece from. Next, chose the tower (1 - #{@towers}) you would like to 
-      move that disk to."
+    puts "Welcome to TOWERS OF HANOI. The goal of the game is to move all the 
+    disks from the leftmost tower to the rightmost tower, adhering to the 
+    following rules: Move only one disk at a time. A larger disk may not be 
+    placed ontop of a smaller disk. To play, chose the tower (1 - #{@towers}) 
+    you would like to move a piece from. Next, chose the tower (1 - #{@towers}) 
+    you would like to move that disk to. 'quit' will quit the program at any 
+    time. Enjoy! :)"
+
     set_up_board
     get_move
   end
 
   #ask player where she would like to move from and to
   def get_move
-    display_board
+    render
     puts "Move FROM?"
-    from = gets.chomp.to_i - 1
+    from = gets.chomp
+    
+    # quit program if player types quit
+    exit if from.downcase == "quit"
 
+    from = from.to_i - 1 #convert user input into usable integer index
+  
     puts "Move TO?"
-    to = gets.chomp.to_i - 1
+    to = gets.chomp
+    
+    # quit program if player types quit
+    exit if to.downcase == "quit"
+    
+    to = to.to_i - 1 #convert user input into usable integer index
 
     if valid_move?(from, to)
       play_move(from, to)
+    
     else
-      puts "NOT VALID. Try again :)"
+      puts "\n\n__________"
+      puts "\nNOT VALID. Try again :)"
       get_move
     end
   end
 
   #define valid move
   def valid_move?(from, to)
-    if from.between?(0, @towers) && to.between?(0, @towers) && from != to && 
-      (@board[to].empty? || @board[from][0] < @board[to][0] )
-      return true
-    else
+
+    #choices are between tower 1 and total number of towers
+    if !from.between?(0, @towers) && !to.between?(0, @towers) 
       return false
+
+    #to and from choice are different from each other
+    elsif from == to 
+      return false
+
+    #from choice is not nil
+    elsif @board[from][0].nil? 
+      return false
+
+    #if to choice is nil, move is valid
+    elsif @board[to][0].nil?
+      return true
+
+    #return false if from choice is greater than to choice
+    elsif @board[from][0] > @board[to][0]
+      return false
+
+    else
+      return true
     end
   end
 
   #manipulate board to reflect player move choise
   def play_move(from, to)
+    #create duplicate of board, to correct pass by reference error
+    #add top disk on from tower to the top of to tower
     to_dup = @board[to].dup
     to_dup.unshift(@board[from][0]) 
     @board[to] = to_dup
     @board[from].shift
   
-    
     if won?
-      display_board
-      puts "congratulations you won!"
+      render
+      puts "congratulations you won!\n\n"
     else
       get_move
     end
   end
 
   #Display the board so that it is easier to visualize
-  def display_board
-    display_board = Array.new(@towers)
+  def render
+    render = Array.new(@towers)
     
     @board.each_with_index do |tower, i|
-      display_board[i] = [" "] * (@towers - tower.length) + tower
+      render[i] = [" "] * (@towers - tower.length) + tower
     end
     
     puts "__________"
     puts "\nTOWERS OF HANOI\n"
     
-    display_board.transpose.each do |row|
+    render.transpose.each do |row|
       puts " " + row.join("    ")
     end
     
@@ -97,5 +130,5 @@ class TowersOfHanoi
 end
 
 
-game = TowersOfHanoi.new(3)
+game = TowersOfHanoi.new(8)
 game.play
